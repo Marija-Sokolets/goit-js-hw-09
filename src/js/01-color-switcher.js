403321ea637e0dcca7c6b1b5028e03c1eea3d1c4
Marija-Sokolets <128020732+Marija-Sokolets@users.noreply.github.com>
2023-08-17 14:48:37 +0300

    js-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,31 @@
+// const bodyStart = document.querySelector('body');
+const startBtn = document.querySelector(`button[data-start]`);
+const stopBtn = document.querySelector(`button[data-stop]`);
+let timerId = null;
+// const color = document.body.style.backgroundColor;
+
+startBtn.addEventListener('click', startColor);
+stopBtn.addEventListener('click', stopColor);
+
+function startColor() {
+  startBtn.disabled = true;
+  // stopBtn.disabled = false;
+
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColor() {
+  // startBtn.disabled = true;
+  startBtn.disabled = false;
+
+  clearInterval(timerId);
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
+
+
